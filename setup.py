84d21875bed7805acd91ff4faf0ec847bf61204b
choo <sugimori.choo@gmail.com>
2021-01-28 12:38:43 +0900

    Update minor version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as f:
 
 setuptools.setup(
     name='fsutils',
-    version='1.0.3',
+    version='1.1.0',
     description='utility functions related to filesystems such as creating file or reading file',
     long_description=long_description,
     author='Sugimori Choo',
